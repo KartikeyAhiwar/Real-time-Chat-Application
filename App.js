d7de65c9e37cf865762ee6316e7a540d91fbd1b1
Kartikey Ahirwar <116839169+KartikeyAhiwar@users.noreply.github.com>
2024-10-09 22:55:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import Chat from './components/Chat';
+
+function App() {
+  return (
+    <div>
+      <h1>Real-Time Chat App</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
 
